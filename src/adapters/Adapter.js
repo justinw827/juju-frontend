565ff712722c6a3d1e1cb4e3750d7b64b6a47949
Justin Wang <justinw0827@gmail.com>
2018-11-20 15:08:11 -0500

    Refactored Adapter

--- src/adapters/Adapter.js
+++ src/adapters/Adapter.js
@@ -1,14 +1,18 @@
 import { PARTIES_URL, PROFILE_URL } from './links'
 
 class Adapter {
+  // Class method for querying Spotify API with user's search term. Actual request to Spoitfy API
+  // performed by back end to avoid CORS issues.
   static getSongs(fetchBody) {
     return this.fetchPost(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/search`, fetchBody)
   }
 
+  // Class method to fetch all parties from backend
   static getAllEvents() {
     return ( fetch(PARTIES_URL).then(r => r.json()) )
   }
 
+  // Class method to fetch all parties belonging to a user
   static getUsersParties() {
     return ( fetch(PROFILE_URL, {
       method: 'GET',
@@ -18,8 +22,8 @@ class Adapter {
     }).then(r => r.json()) )
   }
 
+  // Class method for creating a party to be stored in the backend
   static createEvent(fetchBody) {
-
     const fetchParams = {
       method: "POST",
       headers: {
@@ -29,9 +33,10 @@ class Adapter {
       body: JSON.stringify(fetchBody)
     }
 
-    return fetch(PARTIES_URL, fetchParams).then(r => r.json())
+    return this.fetchPost(PARTIES_URL, fetchBody)
   }
 
+  // Class method to make POST requests to the given endpoint with the given body
   static fetchPost(endpoint, fetchBody) {
     const fetchParams = {
       method: "POST",
