d462441670dc099e43add6aa5d6f0d53a4e3e453
Justin Wang <justinw0827@gmail.com>
2019-06-28 14:24:56 -0400

    switched links to heroku

--- src/adapters/links.js
+++ src/adapters/links.js
@@ -1,3 +1,5 @@
-export const LOGIN_URL = "http://localhost:3000/api/v1/login"
-export const PARTIES_URL = "http://localhost:3000/api/v1/events"
-export const PROFILE_URL = "http://localhost:3000/api/v1/profile"
+//http://localhost:3000
+const domain = "https://juju-backend.herokuapp.com"
+export const LOGIN_URL = domain + "/api/v1/login"
+export const PARTIES_URL = domain + "/api/v1/events"
+export const PROFILE_URL = domain + "/api/v1/profile"
