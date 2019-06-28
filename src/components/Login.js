66bdf19329e9f6c967bd452052ae56bbbd154952
Justin Wang <justinw0827@gmail.com>
2019-06-28 15:00:55 -0400

    switched to heroku link

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import { Button } from 'semantic-ui-react';
 //http://localhost:3000
 //https://juju-backend.herokuapp.com
 // Login button to initiate login through Spotify
-const Login = () => <Button as="a" href="http://localhost:3000/api/v1/login" size="massive" color="instagram">Log in</Button>
+// const Login = () => <Button as="a" href="http://localhost:3000/api/v1/login" size="massive" color="instagram">Log in</Button>
+const Login = () => <Button as="a" href="https://juju-backend.herokuapp.com/api/v1/login" size="massive" color="instagram">Log in</Button>
 
 export default Login
